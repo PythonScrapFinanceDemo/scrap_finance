9a55bad1ea4c79a68b807cdde1a1fdcd7dda7670
superPershing <727342027@qq.com>
2017-01-02 19:33:32 +0800

    error at page 18

--- scrap.py
+++ scrap.py
@@ -65,27 +65,50 @@ for user_i in user_information_obj:
 
 # 获取下一页的按钮，每页12个，下一页是第11个按钮,然后点击
 page = 1
-def next_page(page):
-    next_page_button = driver.find_elements_by_xpath("//a[@class='Pager']")[10]
-    next_page_button.click()
 
-    page_now = int(bsObj.find('select',id="AspNetPager1_input").find('option',selected="true").get_text())
+def get_page_now():  #获取当前页数
+    bsObj = BeautifulSoup(driver.page_source,'html.parser')
+    #page_now = int(bsObj.find('select',id="AspNetPager1_input").find('option',selected="true").get_text())
+    try:
+        page_now = int(bsObj.find('select',id="AspNetPager1_input").find('option',selected="true").get_text())
+    except Exception as e:
+        print("Can't get page_now!")
+    else:
+        return page_now
+
+
+def get_next_page_button():  #返回按钮
+    page_now = get_page_now()
+    page_next = str(page_now + 1)
+    try:
+        if page_now%10 != 0:
+            #next_page_button = driver.find_element_by_xpath("//a[@title='转到第'+page_next+'页']")
+            next_page_button = driver.find_element_by_link_text('['+page_next+']')
+        else:
+            next_page_button = driver.find_element_by_link_text('...')
+    except Exception as e:
+        print("We can't get the next page button!")
+        raise e
+    else:
+        return next_page_button
+
+
+def next_page(page):
+    next_page_button = get_next_page_button()
+    try:
+        next_page_button.click()   #模拟点击
+    except Exception as e:
+        print("blank page_button or other wrong!")
+        raise e
+
+    page_now = get_page_now()
+
     logging.debug('Now page is:%s', page_now)
     page = page + 1
+    logging.debug('The page we record is:%s', page)
+    assert(page==page_now)
+    return page
 
-next_page(page)
-next_page(page)
-next_page(page)
-#
-'''
-test
-driver.find_elements_by_xpath("//a[@class='Pager']")[10].click()
-page = page + 1
-driver.find_elements_by_xpath("//a[@class='Pager']")[10].click()
-page = page + 1
-next_page(page)
-next_page(page)
-next_page(page)
-'''
-
+for i in range(page_length):
+    page = next_page(page)
 logging.debug('End of program')
