d7a72a1792c786512d29eca8fc4bb78e6db9e644
superPershing <727342027@qq.com>
2017-01-15 00:24:17 +0800

    change date any day

--- package_scrap/date.py
+++ package_scrap/date.py
@@ -1,5 +1,7 @@
+# coding:utf-8
 import datetime
 import calendar
+from datetime import date, timedelta as td
 
 def get_date_list(begin_date,end_date):
     '''
@@ -11,35 +13,21 @@ def get_date_list(begin_date,end_date):
     begin_date = begin_date.split('-')
     end_date = end_date.split('-')
     date_list = []
-    begin_year = begin_date[0]
-    begin_month = begin_date[1]
-    begin_day = begin_date[2]
-    end_year = end_date[0]
-    end_month = end_date[1]
-    end_day = end_date[2]
+    begin_year = int(begin_date[0])
+    begin_month = int(begin_date[1])
+    begin_day = int(begin_date[2])
+    end_year = int(end_date[0])
+    end_month = int(end_date[1])
+    end_day = int(end_date[2])
     exclude_date = ['2016-04-02','2016-04-03','2016-04-04','2016-04-30','2016-05-01','2016-05-02','2016-06-09','2016-06-10','2016-06-11','2016-06-12','2016-09-15','2016-09-16','2016-09-17','2016-09-18']
-    if int(begin_year) == int(end_year):
-        total_month = int(end_month) - int(begin_month) + 1
-        for month_id in range(total_month):
-            month_end_day = calendar.monthrange(int(begin_year),month_id+int(begin_month))[1]
-            #获得该月份实际天数
-            for day_id in range(1,month_end_day+1):
-                month_temp = str(month_id+int(begin_month))
-                if len(month_temp) == 1: #必须写成0x的形式
-                    month_temp = '0'+month_temp
-                day_temp = str(day_id)
-                if len(day_temp) == 1:
-                    day_temp = '0'+day_temp
-                date_temp = begin_year+'-'+month_temp+'-'+day_temp
-                #这个网站自动适配日期做的还可以，2016-7-1这样的也是符合要求的
-                #此话作废，我真高估了前端的智商，这个网站就是个垃圾！
-                if date_temp not in exclude_date:
-                    date_list.append(date_temp)
-                #date_list.append(date_temp)
-    else:
-        print("I have to do some change in my function!")
-        print("按理说根据需求不会出现这种情况，如果出现，程序的健壮性需要改善。")
-
+    d_begin = date(begin_year,begin_month,begin_day)
+    d_end = date(end_year,end_month,end_day)
+    delta = d_end - d_begin
+    date_list = []
+    for i in range(delta.days + 1):
+        date_temp = str(d_begin + td(days=i))
+        if date_temp not in exclude_date:
+            date_list.append(date_temp)
     date_list = tick_weekend(date_list)
     return date_list
 
@@ -52,10 +40,9 @@ def tick_weekend(date_list):
             date_without_weekend.append(date)
     return date_without_weekend
 
-
 if __name__ == "__main__":
     #仅供测试
     begin_date = input("begin_date:  ")
-    end_date = input("end_date:   ")
+    end_date = input("begin_date:  ")
     date_list = get_date_list(begin_date,end_date)
     print(date_list)
